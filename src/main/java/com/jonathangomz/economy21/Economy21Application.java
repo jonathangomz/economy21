e854753d7b132a151a37d98bd152b3bd39ce26a7
Jonathan Gomez <jonicgp97@gmail.com>
2025-03-07 13:53:11 -0600

    Initial commit

---
+++ src/main/java/com/jonathangomz/economy21/Economy21Application.java
@@ -0,0 +1,13 @@
+package com.jonathangomz.economy21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Economy21Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Economy21Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/jonathangomz/economy21/Economy21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonathangomz.economy21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Economy21ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
