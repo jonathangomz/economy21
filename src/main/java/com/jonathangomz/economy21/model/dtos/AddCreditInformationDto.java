06a253af941f7512fa818ede29d7281a0b146309
Jonathan Gomez <jonicgp97@gmail.com>
2025-03-21 01:10:01 -0600

    Remove unused @Builder import from AddCreditInformationDto

--- src/main/java/com/jonathangomz/economy21/model/dtos/AddCreditInformationDto.java
+++ src/main/java/com/jonathangomz/economy21/model/dtos/AddCreditInformationDto.java
@@ -6,7 +6,6 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-import lombok.Builder;
 
 import java.math.BigDecimal;
 
