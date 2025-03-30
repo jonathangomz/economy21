2bfbf5bac4a7a8ef94ae8838c04a20f03cdbaaec
Jonathan Gomez <jonicgp97@gmail.com>
2025-03-30 12:33:01 -0600

    Add endpoint for retrieving credit information

--- src/main/java/com/jonathangomz/economy21/controller/AccountCreditController.java
+++ src/main/java/com/jonathangomz/economy21/controller/AccountCreditController.java
@@ -1,11 +1,8 @@
 package com.jonathangomz.economy21.controller;
 
 import com.jonathangomz.economy21.exceptions.ResourceNotFound;
-import com.jonathangomz.economy21.model.Account;
 import com.jonathangomz.economy21.model.AccountCreditInformation;
-import com.jonathangomz.economy21.model.dtos.AddCreditInformationDto;
 import com.jonathangomz.economy21.service.AccountManager;
-import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -20,6 +17,7 @@ public class AccountCreditController {
         this.accountManager = accountManager;
     }
 
+    @GetMapping
     public AccountCreditInformation getCreditInformation(@PathVariable UUID accountId) {
         // TODO: Replace with user id from context
         var owner = UUID.fromString("e7dc9147-7c56-4a41-912d-8c8e9ef3a1e8");
@@ -29,12 +27,5 @@ public class AccountCreditController {
             throw new ResourceNotFound("Credit information not found");
         }
         return account.getCreditInformation();
-    }
-
-    public Account addCreditInformation(@PathVariable UUID accountId, @RequestBody @Valid AddCreditInformationDto dto) {
-        // TODO: Replace with user id from context
-        var owner = UUID.fromString("e7dc9147-7c56-4a41-912d-8c8e9ef3a1e8");
-
-        return this.accountManager.addCreditInformation(owner, accountId, dto);
     }
 }
